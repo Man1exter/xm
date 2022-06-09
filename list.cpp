81b14e582ddc17d85610b1ff3894029e7b4eb7f1
Mariusz Perzynski <m.perzyk@interia.pl>
2022-06-09 17:26:52 +0200

    cmcs

--- list.cpp
+++ list.cpp
@@ -6,15 +6,16 @@
 #include <iostream>
 #include <ctime>
 #include <dos.h>
+
 // utwórz w programie egzemplarze klas Student oraz Pracownik,a następnie (przypisz ich składnikom danych wartości pobrane z klawiatury)
-// Zmodyfikuj program z zadania 1 tak, aby przypisanie składnikom danych wartości pobranych zklawiatury, w stworzonych  egzemplarzach klasStudentorazPracownik,//  //// wykonane zostało zapomocą metody zaprzyjaźnionej z klasą.
+
 using namespace std;
  
 class osoba {
  
 protected:
     string imie = "Mariusz";
-    string nazwisko = "Perzynski";
+    string nazwisko = "Perzyński";
     int wiek = 21;
  
 public:
@@ -29,14 +30,20 @@ void osoba::pokaz_dane() {
  
 class pracownik : private osoba
 {
-friend void pobierz_stanowisko(pracownik&);
 private:
     string stanowisko;
+    void pobierz_dane();
 public:
     void pokaz_dane();
 };
  
+void pracownik::pobierz_dane() {
+    cout << "\nPodaj stanowisko: \n";
+    cin >> stanowisko;
+}
+ 
 void pracownik::pokaz_dane() {
+    pobierz_dane();
     cout << "\nImie: " << imie << "\n";
     cout << "Nazwisko: " << nazwisko << "\n";
     cout << "Wiek: " << wiek << "\n";
@@ -44,15 +51,20 @@ void pracownik::pokaz_dane() {
 }
 class student : private osoba
 {
-friend void pobierz_grupe(student&);
 private:
     string grupa;
+    void pobierz_dane();
 public:
     void pokaz_dane();
 };
  
+void student::pobierz_dane() {
+    cout << "\nPodaj grupe: \n";
+    cin >> grupa;
+}
  
 void student::pokaz_dane() {
+    pobierz_dane();
     cout << "\nImie: " << imie << "\n";
     cout << "Nazwisko: " << nazwisko << "\n";
     cout << "Wiek: " << wiek << "\n";
@@ -66,18 +78,7 @@ int main()
     student p3;
  
     p1.pokaz_dane();
-    pobierz_stanowisko(p2);
     p2.pokaz_dane();
-    pobierz_grupe(p3);
     p3.pokaz_dane();
     return 0;
-}
- 
-void pobierz_stanowisko(pracownik& p) {
-    cout << "\nPodaj stanowisko: \n";
-    cin >> p.stanowisko;
-}
-void pobierz_grupe(student& p) {
-    cout << "\nPodaj grupe: \n";
-    cin >> p.grupa;
 }
